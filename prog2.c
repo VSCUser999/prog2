fd43a14bc1e3f93552467e320746f30e341e831c
VSCUser999 <def8663@gmail.com>
2021-10-04 20:38:30 +0800

    HMUSER

--- prog2.c
+++ prog2.c
@@ -3,25 +3,25 @@
 
 int main (void){
     int v1;
-    printf("是否有房產?(0:No, 1:Yes)");
+    printf("?O?_??????(0:No, 1:Yes)");
     scanf("%d", &v1);
         if (v1 == 1){
-            printf("可以貸款\n");
+            printf("?i?H?U??\n");
             return 0;
         }else if (v1 == 0){
-            printf("是否已婚?(0:No, 1:Yes)");
+            printf("?O?_?w?B?(0:No, 1:Yes)");
             }   
             scanf("%d", &v1);
             if(v1 == 1){
-                printf("可以貸款\n");
+                printf("?i?H?U??\n");
                 return 0;
                 }else if(v1 == 0){
-                    printf("是否年收入 > 100萬?(0:No, 1:Yes)");
+                    printf("?O?_?~???J > 100?U?(0:No, 1:Yes)");
                     }
                     scanf("%d", &v1);
                     if(v1 == 0){
-                        printf("不能貸款\n");
+                        printf("????U??\n");
                     }else if(v1 == 1){
-                        printf("可以貸款\n");
+                        printf("?i?H?U??\n");
                 }
             }
